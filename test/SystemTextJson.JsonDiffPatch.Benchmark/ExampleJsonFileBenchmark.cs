25d0d9293494a88053daf5195aeb5084cf917d58
Wei Chen <weichch87@gmail.com>
2022-04-29 21:58:37 +1200

    Fix benchmark for Linux

--- test/SystemTextJson.JsonDiffPatch.Benchmark/ExampleJsonFileBenchmark.cs
+++ test/SystemTextJson.JsonDiffPatch.Benchmark/ExampleJsonFileBenchmark.cs
@@ -22,7 +22,7 @@ namespace SystemTextJson.JsonDiffPatch.Benchmark
 
         protected static string GetExampleFile(string fileName)
         {
-            return $"Examples\\{fileName}";
+            return $"Examples/{fileName}";
         }
 
         [GlobalSetup]
